eb43f41351277556f87504164c5424f2747258b8
simplykevine <umutonikevine2004@gmail.com>
2024-03-03 17:40:27 +0000

    api

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
 """
-A script that use reddit api to outputs the
-number of subscribers of a certain subreddit.
+Returns the number of subscribers from a subreddit
 """
-
-
-import json
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """ Set a custom header user-agent """
+    headers = {"User-Agent": "ALU-scripting API 0.1"}
+    url = "https://www.reddit.com/r/{}.json".format(subreddit)
 
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
+    try:
+        response = requests.get(url, headers=headers,
+                                timeout=30, allow_redirects=False)
+
+    except requests.exceptions.Timeout:
+        return "The request Timed out"
 
     if response.status_code == 200:
-        data = json.loads(response.text)
-        return data['data']['subscribers']
-
+        json_data = response.json()
+        subscriber_number = (
+            json_data.get("data")
+            .get("children")[0]
+            .get("data")
+            .get("subreddit_subscribers")
+        )
+        return subscriber_number
+    elif response.status_code == 404:
+        return 0
     else:
         return 0
