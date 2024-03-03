762cda2f35ec0c2c709ccf5afc301b88e91f5fbc
simplykevine <umutonikevine2004@gmail.com>
2024-03-03 09:11:53 -0800

    api

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,19 +1,36 @@
 #!/usr/bin/python3
-"""DOC"""
-import requests
+
+"""
+Query a subreddit and return the number of
+total subscribers in that subredit
+"""
+
+from requests import get
+from sys import argv
 
 
-def number_of_subscribers(subreddit):
-    """DOC"""
-    reddit_url = "https://www.reddit.com/r/{}/about.json" \
-        .format(subreddit)
+headers = {
+    "User-Agent": "Of course I had to use a custom User-Agent",
+    "X-Forwared-For": "iamthecavalry"
+}
 
-    header = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(reddit_url,
-                            headers=header
-                            )
-    if response.status_code == 200:
-        data = response.json()['data']
-        subs = data['subscribers']
-        return subs
-    return 0
+
+def number_of_subscribers(subreddit: str) -> int:
+    """
+    Query the subreddit and return the number of
+    Active subs. If its an invalid subredit, return 0
+    """
+    response = get("https://www.reddit.com/r/{}/about.json".format(subreddit),
+                   headers=headers)
+    data = response.json()
+    try:
+        if 'error' in data.keys():
+            return 0
+        else:
+            return data['data']['subscribers']
+    except Exception as e:
+        return 0
+
+
+if __name__ == "__main__":
+    print(number_of_subscribers(argv[1]))
